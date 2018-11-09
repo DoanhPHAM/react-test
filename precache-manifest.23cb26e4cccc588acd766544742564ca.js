3b45924724922b668e3f826dbd1a0b4e4961a692
DoanhPHAM <pham.van.doanh@framgia.com>
2018-11-09 13:23:56 +0700

    Updates

---
+++ precache-manifest.23cb26e4cccc588acd766544742564ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48517a86b94fbd3dbff65f08414b8e6d",
+    "url": "/react-test/static/media/retro.48517a86.svg"
+  },
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "fa4ac7da722ff579cb2d",
+    "url": "/react-test/static/js/main.fa4ac7da.chunk.js"
+  },
+  {
+    "revision": "53c3927d3f5d83452c16",
+    "url": "/react-test/static/js/1.53c3927d.chunk.js"
+  },
+  {
+    "revision": "fa4ac7da722ff579cb2d",
+    "url": "/react-test/static/css/main.d5e1abc0.chunk.css"
+  },
+  {
+    "revision": "53c3927d3f5d83452c16",
+    "url": "/react-test/static/css/1.4be7c2ed.chunk.css"
+  },
+  {
+    "revision": "240e13a6ee78e4eded349d1f693837c7",
+    "url": "/react-test/index.html"
+  }
+];
